dbd91803011a81b285b80708a7cd26c1422c1860
roaaayman <aymanroaa025@gmail.com>
2025-06-08 20:31:02 +0300

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,22 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AllProductComponent } from './products/componets/all-product/all-product.component';
-import { ProductDetailesComponent } from './products/componets/product-detailes/product-detailes.component';
+
+
 import { CartComponent } from './carts/components/cart/cart.component';
 import { CheckoutComponent } from './carts/components/checkout/checkout.component';
 import { HomeComponent } from './home/home.component';
+import { AllProductComponent } from './products/componets/all-product/all-product.component';
+import { ProductDetailesComponent } from './products/componets/product-detailes/product-detailes.component';
 
 const routes: Routes = [
-  {path:"home", component: HomeComponent},
-  {path:"products", component: AllProductComponent},
-  {path:"products/:id", component: ProductDetailesComponent},
-  {path:"cart", component: CartComponent},
-  {path:"checkout", component: CheckoutComponent},
-  {path:"", redirectTo: "home", pathMatch: "full"},
-  {path:"**", redirectTo: "home", pathMatch: "full"}
+  { path: "home", component: HomeComponent },
+  { path: "products", component: AllProductComponent },
+  { path: "products/:id", component: ProductDetailesComponent },
+  { path: "cart", component: CartComponent },
+  { path: "checkout", component: CheckoutComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "**", redirectTo: "/home", pathMatch: "full" }
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
